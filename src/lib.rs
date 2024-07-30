a73b0f5c2d40bc60665a980ee2dc43a50b85108b
Bruce Cutler <bruce@sumebrius.net>
2024-07-30 21:24:27 +1200

    Basic constructor

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,34 @@
 use pyo3::prelude::*;
+use std::fs::read;
+use std::path::PathBuf;
+
+#[pyclass]
+struct WavFile {
+    #[pyo3(get)]
+    bytes: Vec<u8>,
+}
+
+#[derive(FromPyObject)]
+enum ConstructorArg {
+    Bytes(Vec<u8>),
+    Path(PathBuf),
+    // TODO - Accept a `read`able file
+}
+
+#[pymethods]
+impl WavFile {
+    #[new]
+    fn new(file: ConstructorArg) -> PyResult<Self> {
+        let bytes: Vec<u8> = match file {
+            ConstructorArg::Bytes(bytes) => bytes,
+            ConstructorArg::Path(path) => read(path)?,
+        };
+        Ok(WavFile { bytes })
+    }
+}
+
+#[pymodule]
+fn pywav(m: &Bound<'_, PyModule>) -> PyResult<()> {
+    m.add_class::<WavFile>()?;
+    Ok(())
+}
