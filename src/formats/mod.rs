f5e28a9a9b9902875591b184876e8044b05eee2d
Bruce Cutler <bruce@sumebrius.net>
2024-08-10 18:40:06 +1200

    Use format codes as the enum discriminants

--- src/formats/mod.rs
+++ src/formats/mod.rs
@@ -1,26 +1,21 @@
+use num_enum::TryFromPrimitive;
 use pyo3::prelude::*;
 
-#[pyclass(eq, eq_int, frozen, get_all)]
-#[derive(PartialEq, Clone, Copy)]
+#[pyclass(eq, eq_int, frozen, get_all, rename_all = "SCREAMING_SNAKE_CASE")]
+#[derive(PartialEq, Clone, Copy, TryFromPrimitive)]
+#[repr(u16)]
 pub enum Format {
-    Pcm,
-    IeeeFloat,
-    Alaw,
-    Mulaw,
-    Extensible,
+    Pcm = 0x0001,
+    IeeeFloat = 0x0003,
+    Alaw = 0x0006,
+    Mulaw = 0x0007,
+    Extensible = 0xFFFE,
     Unknown,
 }
 
 impl Format {
     pub fn from_bytes(bytes: &[u8; 2]) -> Self {
-        match u16::from_ne_bytes(*bytes) {
-            0x0001 => Self::Pcm,
-            0x0003 => Self::IeeeFloat,
-            0x0006 => Self::Alaw,
-            0x0007 => Self::Mulaw,
-            0xFFFE => Self::Extensible,
-            _ => Self::Unknown,
-        }
+        Self::try_from(u16::from_ne_bytes(*bytes)).unwrap_or(Self::Unknown)
     }
 
     pub fn requires_fact_chunk(&self) -> bool {
