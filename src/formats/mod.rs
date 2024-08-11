b111559e743044a5cfcb57603f5141c624f29576
Bruce Cutler <bruce@sumebrius.net>
2024-08-11 23:33:40 +1200

    Remove superfluous member rename

--- src/formats/mod.rs
+++ src/formats/mod.rs
@@ -1,14 +1,7 @@
 use num_enum::TryFromPrimitive;
 use pyo3::prelude::*;
 
-#[pyclass(
-    eq,
-    eq_int,
-    frozen,
-    get_all,
-    rename_all = "SCREAMING_SNAKE_CASE",
-    module = "waveinfo"
-)]
+#[pyclass(eq, eq_int, frozen, get_all, module = "waveinfo")]
 #[derive(PartialEq, Clone, Copy, TryFromPrimitive)]
 #[repr(u16)]
 #[allow(clippy::upper_case_acronyms, non_camel_case_types)]
